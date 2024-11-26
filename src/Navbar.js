e1e24842b01c4c853c2b955725570aa99aa5c317
jinhy <wlsdustm92@gmail.com>
2024-11-27 00:15:43 +0900

    style: Navbar css 파일 분리

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,33 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <header style={styles.navbar}>
-      <Link to="/signin" style={styles.navLink}>
-        <div style={styles.logo}>HARU NOTE</div>
+    <header className="navbar">
+      <Link to="/signin" className="navLink">
+        <div className="logo">HARU NOTE</div>
       </Link>
 
       <nav>
-        <ul style={styles.navList}>
-          <div style={styles.navLine}></div>
-          <li style={styles.navItem}>
-            <Link to="/calendar" style={styles.navLink}>
+        <ul className="navList">
+          <div className="navLine"></div>
+          <li className="navItem">
+            <Link to="/calendar" className="navLink">
               Calendar
             </Link>
           </li>
-          <li style={styles.navItem}>
-            <Link to="/blog" style={styles.navLink}>
+          <li className="navItem">
+            <Link to="/blog" className="navLink">
               Blog
             </Link>
           </li>
-          <li style={styles.navLinkWrapper}>
-            <Link to="/signup" style={styles.navLink}>
+          <li className="navLinkWrapper">
+            <Link to="/signup" className="navLink">
               Sign up
             </Link>
           </li>
-          <li style={styles.navItem2}>
-            <Link to="/signin" style={styles.navLinkSignin}>
+          <li className="navItem2">
+            <Link to="/signin" className="navLinkSignin">
               Sign in
             </Link>
           </li>
@@ -36,87 +37,5 @@ function Navbar() {
     </header>
   );
 }
-
-const styles = {
-  navbar: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#DCDAD8",
-    padding: "10px 20px",
-    color: "black",
-    height: "50px",
-    margin: "0 auto",
-  },
-
-  logo: {
-    fontSize: "24px",
-    fontWeight: "lighter",
-    marginLeft: "50px",
-  },
-  navList: {
-    display: "flex",
-    listStyleType: "none",
-    padding: "11px 0px",
-    backgroundColor: "white",
-    borderRadius: "50px",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    width: "180%",
-    justifyContent: "right",
-    alignItems: "center",
-    transform: "translateX(-46%)",
-    height: "20px",
-    position: "relative",
-  },
-  navLine: {
-    position: "absolute",
-    top: "54%",
-    left: "-23%",
-    width: "68%",
-    height: "1px",
-    backgroundColor: "#000000",
-    transform: "translateY(-50%)",
-  },
-  navItem: {
-    listStyle: "none",
-    margin: "0 50px",
-  },
-  navItem2: {
-    listStyle: "none",
-    marginLeft: "50px",
-    marginRight: "10px",
-  },
-  navLinkWrapper: {
-    display: "inline-block",
-    backgroundColor: "#fff",
-    color: "#333",
-    padding: "3px 10px",
-    borderRadius: "25px",
-    textDecoration: "none",
-    textAlign: "center",
-    border: "1px solid #000",
-    width: "80px",
-    marginLeft: "10px",
-  },
-  navLinkText: {
-    fontSize: "18px",
-  },
-  navLink: {
-    color: "black",
-    textDecoration: "none",
-    fontSize: "18px",
-  },
-  navLinkSignin: {
-    display: "inline-block",
-    backgroundColor: "black",
-    color: "white",
-    padding: "6px 30px",
-    borderRadius: "25px",
-    textDecoration: "none",
-    textAlign: "center",
-    fontWeight: "normal",
-    fontSize: "18px",
-  },
-};
 
 export default Navbar;
