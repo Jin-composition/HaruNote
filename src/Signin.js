90a30ed1bd9bc99976ed3afc050b539306424af1
jinhy <wlsdustm92@gmail.com>
2024-11-27 00:13:39 +0900

    style: Signup, Signin css 파일 분리

--- src/Signin.js
+++ src/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import title from "./assets/title.png";
+import "./Signin.css";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
@@ -12,48 +13,30 @@ const Signin = () => {
 
   return (
     <div>
-      <main
-        style={{
-          height: "720px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "NEXON Lv1 Gothic Low OTF",
-          marginTop: "-220px",
-          fontSize: "30px",
-          flexDirection: "column",
-        }}
-      >
-        <img
-          src={title}
-          style={{ width: "800px", marginTop: "25%" }}
-          alt="description"
-        />
-        {/* <p>소소한 일상을 나누는 공간, 하루노트</p> */}
-        <div style={styles.container}>
+      <main className="signin-main">
+        <img src={title} className="signin-title" alt="description" />
+        <div className="signin-container">
           <input
             type="email"
             placeholder="name@company.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={styles.input}
+            className="signin-input"
           />
 
-          <div style={styles.passwordContainer}>
+          <div className="password-container">
             <input
               type="password"
               placeholder="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              style={styles.passwordInput}
+              className="password-input"
             />
 
             <button
               type="button"
               onClick={handleSignin}
-              style={styles.signinButton}
-              onMouseOver={(e) => (e.target.style.backgroundColor = "#444")}
-              onMouseOut={(e) => (e.target.style.backgroundColor = "black")}
+              className="signin-button"
             >
               Sign in
             </button>
@@ -62,56 +45,6 @@ const Signin = () => {
       </main>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    maxWidth: "400px",
-    padding: "20px",
-    backgroundColor: "#fff",
-    marginTop: "70px",
-    position: "relative",
-  },
-  input: {
-    padding: "17px 25px",
-    marginBottom: "30px",
-    width: "80%",
-    borderRadius: "30px",
-    border: "1px solid black",
-    fontSize: "13px",
-  },
-  passwordContainer: {
-    position: "relative",
-    width: "80%",
-  },
-  passwordInput: {
-    padding: "17px 25px",
-    marginBottom: "15px",
-    width: "100%",
-    borderRadius: "30px",
-    border: "1px solid black",
-    fontSize: "13px",
-    position: "relative",
-    left: "-20px",
-  },
-  signinButton: {
-    position: "absolute",
-    right: "-32px",
-    top: "41%",
-    transform: "translateY(-50%)",
-    padding: "15px 30px",
-    backgroundColor: "black",
-    color: "white",
-    border: "1px solid black",
-    borderRadius: "30px",
-    fontSize: "16px",
-    cursor: "pointer",
-    transition: "background-color 0.3s",
-  },
 };
 
 export default Signin;
--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import signup from "./assets/signup.png";
+import "./Signup.css";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -13,55 +14,37 @@ const Signup = () => {
 
   return (
     <div>
-      <main
-        style={{
-          height: "720px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "NEXON Lv1 Gothic Low OTF",
-          marginTop: "-220px",
-          fontSize: "30px",
-          flexDirection: "column",
-        }}
-      >
-        <img
-          src={signup}
-          style={{ width: "500px", marginTop: "25%" }}
-          alt="description"
-        />
-        {/* <p>나만의 일기장 만들기</p> */}
-        <div style={styles.container}>
+      <main className="main-container">
+        <img src={signup} className="signup-img" alt="description" />
+        <div className="container">
           <input
             type="text"
             placeholder="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            style={styles.input}
+            className="input"
           />
           <input
             type="email"
             placeholder="name@company.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={styles.input}
+            className="input"
           />
 
-          <div style={styles.passwordContainer}>
+          <div className="passwordContainer">
             <input
               type="password"
               placeholder="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              style={styles.passwordInput}
+              className="passwordInput"
             />
 
             <button
               type="button"
               onClick={handleSignin}
-              style={styles.signinButton}
-              onMouseOver={(e) => (e.target.style.backgroundColor = "#444")}
-              onMouseOut={(e) => (e.target.style.backgroundColor = "black")}
+              className="signinButton"
             >
               Sign up
             </button>
@@ -70,56 +53,6 @@ const Signup = () => {
       </main>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    maxWidth: "400px",
-    padding: "20px",
-    backgroundColor: "#fff",
-    marginTop: "40px",
-    position: "relative",
-  },
-  input: {
-    padding: "17px 25px",
-    marginBottom: "20px",
-    width: "80%",
-    borderRadius: "30px",
-    border: "1px solid black",
-    fontSize: "13px",
-  },
-  passwordContainer: {
-    position: "relative",
-    width: "80%",
-  },
-  passwordInput: {
-    padding: "17px 25px",
-    marginBottom: "15px",
-    width: "100%",
-    borderRadius: "30px",
-    border: "1px solid black",
-    fontSize: "13px",
-    position: "relative",
-    left: "-20px",
-  },
-  signinButton: {
-    position: "absolute",
-    right: "-32px",
-    top: "41%",
-    transform: "translateY(-50%)",
-    padding: "15px 30px",
-    backgroundColor: "black",
-    color: "white",
-    border: "1px solid black",
-    borderRadius: "30px",
-    fontSize: "16px",
-    cursor: "pointer",
-    transition: "background-color 0.3s",
-  },
 };
 
 export default Signup;
